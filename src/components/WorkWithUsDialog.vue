562a33321d0044013ed2411d5ba929ee1a91fe14
Vercixxx <krz.sluzalek@gmail.com>
2024-03-25 12:29:24 +0100

    Refactor WorkWithUsDialog.vue and add Firebase storage functionality.  Now you can send CV and it will be storaged

--- src/components/WorkWithUsDialog.vue
+++ src/components/WorkWithUsDialog.vue
@@ -8,6 +8,7 @@
                         <span class="headline">Join out team!</span>
                     </v-col>
                     <v-col cols="2" align="end">
+
                         <v-btn icon="mdi-close" variant="plain" @click="closeWorkWithUsDialog"></v-btn>
                     </v-col>
                 </v-row>
@@ -15,8 +16,36 @@
 
             <v-card-text>
                 <v-form v-model="form">
-                    <v-text-field v-for="field in fields" variant="outlined" :label="field.label"
-                        :rules="field.rules" class="mb-5"></v-text-field>
+                    <!-- Name, email, phone -->
+                    <v-text-field v-for="field in fields" v-model="field.value" variant="outlined" :label="field.label"
+                        :rules="field.rules" class="mb-5" :disabled="loading"></v-text-field>
+                    <!-- Name, email, phone -->
+
+
+                    <!-- Facility -->
+                    <v-select v-model="selectedFacility" :items="getFacilities" item-title="Name" item-value="Name"
+                        label="Choose facility" variant="outlined" :rules="requiredRule" :disabled="loading"
+                        class="mb-5"></v-select>
+                    <!-- Facility -->
+
+                    <!-- Message -->
+                    <v-textarea v-model="message" label="Tell us about yourself" variant="outlined" class="mb-5"
+                        :disabled="loading" auto-grow clearable counter></v-textarea>
+                    <!-- Message -->
+
+                    <!-- CV -->
+                    <v-file-input v-model="file" label="Upload CV" placeholder="Upload your documents"
+                        prepend-icon="mdi-paperclip" show-size variant="outlined" :disabled="loading"
+                        accept="application/pdf" :rules="fileRules">
+                        <template v-slot:selection="{ fileNames }">
+                            <template v-for="fileName in fileNames" :key="fileName">
+                                <v-chip class="me-2" color="primary" size="small" label>
+                                    {{ fileName }}
+                                </v-chip>
+                            </template>
+                        </template>
+                    </v-file-input>
+                    <!-- CV -->
                 </v-form>
             </v-card-text>
 
@@ -27,7 +56,8 @@
                             Fill required fields
                         </v-tooltip>
                         <span>
-                            <v-btn block text="Join" :color="form ? 'success' : 'error'" :disabled="!form" variant="elevated"></v-btn>
+                            <v-btn block text="Join" :color="form ? 'success' : 'error'" :disabled="!form"
+                                variant="elevated" :loading="loading" @click="sendApplication"></v-btn>
                         </span>
                     </v-col>
                 </v-row>
@@ -39,28 +69,44 @@
 <script>
 import { mapState, mapActions, mapGetters } from 'vuex'
 
+import { collection, setDoc, doc } from "firebase/firestore";
+import { db, storage } from '../firebase.js'
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
 export default {
     name: 'WorkWithUsDialog',
 
     computed: {
-        ...mapGetters(['showWorkWithUsDialog'])
+        ...mapGetters(['showWorkWithUsDialog', 'getFacilities'])
     },
 
-    methods: {
-        ...mapActions(['openWorkWithUsDialog', 'closeWorkWithUsDialog']),
-    },
 
     data() {
         return {
             form: false,
 
-            facilities: [],
             selectedFacility: null,
+            message: '',
+
+            file: [],
+            fileUrl: null,
+            fileRules: [
+                value => {
+                    return !value || !value.length || value[0].size < 3000000 || 'Avatar size should be less than 3 MB!'
+                },
+            ],
+
+            loading: false,
+
+            requiredRule: [
+                v => !!v || 'Field required'
+            ],
 
             fields: [
                 {
                     name: 'name',
                     label: 'Name',
+                    value: '',
                     rules: [
                         v => !!v || 'Name is required'
                     ]
@@ -68,6 +114,7 @@ export default {
                 {
                     name: 'email',
                     label: 'Email',
+                    value: '',
                     rules: [
                         v => !!v || 'Email is required',
                         v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
@@ -76,20 +123,77 @@ export default {
                 {
                     name: 'phone',
                     label: 'Phone',
+                    value: '',
                     rules: [
                         v => !!v || 'Phone is required',
                         v => (v && v.length >= 9 && v.length <= 15) || 'Phone must be valid'
                     ]
                 },
-                {
-                    name: 'message',
-                    label: 'Message',
-                    rules: [
-                        v => !!v || 'Message is required'
-                    ]
-                }
             ]
         }
-    }
+    },
+
+
+    methods: {
+        ...mapActions(['triggerAlert', 'closeWorkWithUsDialog']),
+
+        async sendApplication() {
+            this.loading = true;
+
+            try {
+
+                if (this.file) {
+                    const file = this.file[0];
+                    const fileRef = ref(storage, 'Applications/' + this.fields.find(field => field.name === 'email').value + '/' + file.name);
+                    await uploadBytes(fileRef, file);
+
+                    this.fileUrl = await getDownloadURL(fileRef);
+                };
+
+
+
+                const data = {
+                    name: this.fields.find(field => field.name === 'name').value,
+                    email: this.fields.find(field => field.name === 'email').value,
+                    phone: this.fields.find(field => field.name === 'phone').value,
+                    facility: this.selectedFacility,
+                    message: this.message,
+                    fileUrl: this.fileUrl
+
+                };
+
+                const reponse = await setDoc(doc(collection(db, 'Applications'), data.email), {
+                    name: data.name,
+                    email: data.email,
+                    phone: data.phone,
+                    facility: data.facility,
+                    message: data.message,
+                    fileUrl: data.fileUrl
+                });
+
+                this.triggerAlert({
+                    message: 'Application sent successfully!',
+                    type: 'success'
+                });
+
+            } catch (error) {
+                console.error('Error occured while sending application', error);
+                this.triggerAlert({
+                    message: 'Error occured while sending application, please try again later',
+                    type: 'error'
+                });
+
+            }
+
+            this.loading = false;
+            this.closeWorkWithUsDialog();
+        },
+
+
+        exitDialog() {
+            this.fields.forEach(field => field.value = '');
+            this.closeWorkWithUsDialog;
+        }
+    },
 }
 </script>
