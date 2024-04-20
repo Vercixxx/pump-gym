5a57ae405ee1f957d976eb6c3f74d0a740018291
Vercixxx <krz.sluzalek@gmail.com>
2024-04-20 21:18:27 +0200

    Updated dist

--- dist/assets/Footer-BjvKU3Gc.js
+++
@@ -1,1 +0,0 @@
-import{Q as f}from"./index-BuOyHiCi.js";export{f as default};
---
+++ dist/assets/Footer-Crsv6bjO.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./Footer.vue_vue_type_script_setup_true_lang-Bmxq1aRY.js";import"./index-D1Aaj6Hj.js";export{m as default};
